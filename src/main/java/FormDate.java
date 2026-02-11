b35af1111441674384aad086fd9e7cb102e8c131
Grigory  Dubikov <grin-dubikov@yandex.ru>
2026-02-11 22:34:06 +0400

    The code has been formatted

--- src/main/java/FormDate.java
+++ src/main/java/FormDate.java
@@ -1,6 +1,6 @@
 public class FormDate {
 
-        public String day;
-        public String month;
-        public String year;
-    }
+    public String day;
+    public String month;
+    public String year;
+}
